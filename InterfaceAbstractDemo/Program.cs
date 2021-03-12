1b01d012b49c8e6cf847361c609f6b9f9e0b6567
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-12 15:38:02 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                NationalityId = "10000000000",
+                FirstName = "Galip",
+                LastName = "Doğan",
+                DateOfBirth = new DateTime(1986, 11, 4)
+            }) ;
+            Console.ReadLine();
+        }
+    }
+
+
+}
